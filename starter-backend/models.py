39fcfe0ea25939ea3bf6a31370de7669d901c12b
johntnjlu <johntnjlu@gmail.com>
2024-08-29 23:24:17 -0700

    connect model to frontend

--- starter-backend/models.py
+++ starter-backend/models.py
@@ -1,12 +1,43 @@
-from typing import Optional
-from pydantic import BaseModel, Field
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-# Create a Person model
-class Person(BaseModel):
+class Module(nn.Module):
+    def __init__(self, hidden_size):
+        super(Module, self).__init__()
+        self.conv1 = nn.Conv2d(hidden_size, hidden_size, 3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(hidden_size, hidden_size, 3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(hidden_size)
+        self.bn2 = nn.BatchNorm2d(hidden_size)
+        self.activation1 = nn.SELU()
+        self.activation2 = nn.SELU()
 
-    #  The id field is optional, and will be set by the database,
-    #  so it is set to None by default (in the case that the person
-    #  is being created).
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
-    age: int
+    def forward(self, x):
+        x_input = torch.clone(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.activation1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = x + x_input
+        x = self.activation2(x)
+        return x
+
+class ChessNet(nn.Module):
+    def __init__(self, hidden_layers=6, hidden_size=600):
+        super(ChessNet, self).__init__()
+        self.hidden_layers = hidden_layers
+        self.input_layer = nn.Conv2d(6, hidden_size, 3, stride=1, padding=1)
+        self.module_list = nn.ModuleList([Module(hidden_size) for _ in range(hidden_layers)])
+        self.output_layer = nn.Conv2d(hidden_size, 2, 3, stride=1, padding=1)
+
+    def forward(self, x):
+        x = self.input_layer(x)
+        x = F.relu(x)
+        for i in range(self.hidden_layers):
+            x = self.module_list[i](x)
+        x = self.output_layer(x)
+        return x
+    
+
+
